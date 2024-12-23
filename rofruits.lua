7d78e9276c420b4031f31ce6724b7e63d050c455
Lanna <116771638+lanna-official@users.noreply.github.com>
2024-12-24 00:52:30 +0200

    log

--- rofruits.lua
+++ rofruits.lua
@@ -42,9 +42,60 @@ local fruits_but = fruits_tab:CreateButton({
 	Callback = function()
 		ui_lib:Notify({
 			Title = "X-RAY Activé",
-			Content = "Le X-RAY a était activer!",
+			Content = "Le X-RAY a était activé!",
 			Duration = 6.5,
 			Image = "rewind",
 		})
+	end,
+})
+
+local fruits_tog = fruits_tab:CreateToggle({
+	Name = "Auto Stock",
+	Color = Color3.fromRGB(255, 255, 0),
+	Flag = "AutoStock_Flag",
+	Callback = function(value)
+		
+	end,
+})
+
+local fruits_sli = fruits_tab:CreateSlider({
+	Name = "Distance Fruits",
+	Range = {0, 5000},
+	Increment = 100,
+	Suffix = "Distance des fruits",
+	Flag = "FruitsDistance_Flag",
+	Calllback = function(value)
+		
+	end,
+})
+
+local fruits_col = fruits_tab:CreateColorPicker({
+	Name = "Fruits Colors",
+	Color = Color3.fromRGB(255, 255, 0),
+	Flag = "FruitsColors_Flag",
+	Callback = function(value)
+		
+	end,
+})
+
+local fruits_int = fruits_tab:CreateInput({
+	Name = "Nom de fruit",
+	CurrentValue = "",
+	Placeholdertext = "Nom de fruits",
+	RemoveTextAfterFocusLost = false,
+	Flag = "FruitsName_Flag",
+	Callback = function(text)
+		
+	end,
+})
+
+local fruits_dro = fruits_tab:CreateDropdown({
+	Name = "Fruits",
+	Options = {"Light", "Gas", "Smoke"},
+	CurrentOption = {"Light"},
+	MultipleOptions = false,
+	Flag = "FruitsDropdown_Flag",
+	Callback = function(options)
+		
 	end,
 })
